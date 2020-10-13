b43a96d6f3ffb6e6c21dbb3629251319283a76e8
Erwan <erwanletutour.elt@gmail.com>
2020-10-13 09:38:52 +0200

    get weather forecast on page load if navigator geolocation is ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {LocationForm} from './form/form';
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {Forecast} from './forecast/forecast';
@@ -9,10 +9,11 @@ import {latLng, tileLayer} from 'leaflet';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   constructor(private httpClient: HttpClient) {
   }
+
 
   model = new LocationForm('');
 
@@ -23,6 +24,39 @@ export class AppComponent {
   forecast = new Forecast();
 
   options = {};
+
+  ngOnInit(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const url = 'http://localhost:8080/eweather/forecastLocation';
+
+        const optionRequete = {
+          headers: new HttpHeaders({
+            'Access-Control-Allow-Origin': '*'
+          }),
+          params: new HttpParams()
+            .set('latitude', position.coords.latitude.toString())
+            .set('longitude', position.coords.longitude.toString())
+        };
+
+        this.httpClient.get<Forecast>(url, optionRequete).subscribe((response) => {
+          this.forecast = response;
+          this.model.location = '';
+          this.display = true;
+
+          this.options = {
+            layers: [
+              tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; OpenStreetMap contributors'
+              })
+            ],
+            zoom: 7,
+            center: latLng(this.forecast.latitude, this.forecast.longitude)
+          };
+        });
+      });
+    }
+  }
 
   onSubmit(f) {
     console.log(this.url + f);
